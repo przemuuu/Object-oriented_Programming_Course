0ce6f56d427716bbf60d60c81d8dfbe977a394aa
przemuuu <przemsonovsky@gmail.com>
2023-10-18 22:21:33 +0200

    lab1

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,31 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.MovDir;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.print("system wystartował \n");
+        MovDir.MoveDirection[] directions = OptPar.OptionsParser(args);
+        run(directions);
+        System.out.println("system zakończył działanie");
+    }
+    public static void run(MovDir.MoveDirection[] directions) {
+        for (MovDir.MoveDirection direction : directions) {
+            switch(direction) {
+                case FORWARD:
+                    System.out.println("zwierzak idzie do przodu");
+                    break;
+                case BACKWARD:
+                    System.out.println("zwierzak idzie do tyłu");
+                    break;
+                case RIGHT:
+                    System.out.println("zwierzak skręca w prawo");
+                    break;
+                case LEFT:
+                    System.out.println("zwierzak skręca w lewo");
+                    break;
+                default: continue;
+            }
+        }
+    }
+}
