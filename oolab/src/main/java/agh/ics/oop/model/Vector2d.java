37030f6f6b0b165421ac6aaf6009b13059df5b05
przemuuu <przemsonovsky@gmail.com>
2023-10-31 20:23:38 +0100

    lab2

--- oolab/src/main/java/agh/ics/oop/model/Vector2d.java
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -1,5 +1,7 @@
 package agh.ics.oop.model;
 
+import java.util.Objects;
+
 public class Vector2d {
     private final int x;
     private final int y;
@@ -55,5 +57,18 @@ public class Vector2d {
     public Vector2d opposite() {
         Vector2d created = new Vector2d(this.y,this.x);
         return created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector2d vector2d = (Vector2d) o;
+        return x == vector2d.x && y == vector2d.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
