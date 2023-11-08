78ad2f151f3ebf02378e45dd2d2264c8c39640ad
Przemysław Popowski <139262508+przemuuu@users.noreply.github.com>
2023-11-08 22:05:09 +0100

    fixed MoveDirection in World.java

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,16 +1,16 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.MovDir;
+import agh.ics.oop.model.MoveDirection;
 
 public class World {
     public static void main(String[] args) {
         System.out.print("system wystartował \n");
-        MovDir.MoveDirection[] directions = OptionsParser.OptPar(args);
+        MoveDirection[] directions = OptionsParser.parse(args);
         run(directions);
         System.out.println("system zakończył działanie");
     }
-    public static void run(MovDir.MoveDirection[] directions) {
-        for (MovDir.MoveDirection direction : directions) {
+    public static void run(MoveDirection[] directions) {
+        for (MoveDirection direction : directions) {
             switch(direction) {
                 case FORWARD:
                     System.out.println("zwierzak idzie do przodu");
