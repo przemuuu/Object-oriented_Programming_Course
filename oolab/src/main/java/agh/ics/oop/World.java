7d527f2c904068b8c198a873492730a02a262939
Przemysław Popowski <139262508+przemuuu@users.noreply.github.com>
2023-11-09 12:25:19 +0100

    cleaned World.java from unused commands

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -9,30 +9,9 @@ public class World {
     public static void main(String[] args) {
         System.out.print("system wystartował \n");
         List<MoveDirection> directions = OptionsParser.parse(args);
-        //run(directions);
         List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
         Simulation simulation = new Simulation(positions, directions);
         simulation.run();
         System.out.println("system zakończył działanie");
-    }
-
-
-    public static void run(List<MoveDirection> directions) {
-        for (MoveDirection direction : directions) {
-            switch(direction) {
-                case FORWARD:
-                    System.out.println("zwierzak idzie do przodu");
-                    break;
-                case BACKWARD:
-                    System.out.println("zwierzak idzie do tyłu");
-                    break;
-                case RIGHT:
-                    System.out.println("zwierzak skręca w prawo");
-                    break;
-                case LEFT:
-                    System.out.println("zwierzak skręca w lewo");
-                    break;
-            }
-        }
     }
 }
